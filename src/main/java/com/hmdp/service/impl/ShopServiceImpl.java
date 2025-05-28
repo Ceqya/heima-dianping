1f14886d0117c04d0fe28beb463071b45c2ccc7c
ceqya <2766674497@qq.com>
2025-05-28 23:23:07 +0800

    黑马点评缓存穿透代码

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -10,8 +10,13 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisConstants;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_NULL_TTL;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
  * <p>
@@ -38,16 +43,41 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+        //命中的是否是空值,前面判断的是有值，放进来了空字符串和不存在的shopjson，如果不空，则为空字符穿
+        if (shopJson != null) {
+            // 返回错误信息
+            return Result.fail("商铺不存在");
+        }
+
         //4.如果不存在，根据id查询数据库
         Shop shop = getById(id);
         //5.不存在，返回错误信息
         if (shop == null) {
+            //将空值写入redis，防止缓存穿透
+            stringRedisTemplate.opsForValue().set(key, "",CACHE_NULL_TTL, TimeUnit.MINUTES);
+
             return Result.fail("商铺不存在");
         }
         //6.如果存在，将数据写入redis
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
         //7.返回商铺信息
 
         return Result.ok(shop);
+    }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        String id= shop.getId().toString();
+        if(StrUtil.isBlank(id)){
+            return Result.fail("商铺id不能为空");
+        }
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+
+        //1.更新数据库
+        updateById(shop);
+        //2.删除redis缓存
+        stringRedisTemplate.delete(key);
+        return Result.ok();
     }
 }
