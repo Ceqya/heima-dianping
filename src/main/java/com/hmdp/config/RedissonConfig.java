db96372a8b7caba24a216a92e0461bd81305000b
ceqya <2766674497@qq.com>
2025-05-31 00:08:15 +0800

    黑马点评秒杀初步优化取消docker中的redis集群代码

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -19,25 +19,25 @@ public class RedissonConfig {
         // 创建RedissonClient实例
         return Redisson.create(config);
     }
-    @Bean
-    public RedissonClient redissonClient2(){
-        // 创建配置
-        Config config = new Config();
-        // 使用单机模式
-        config.useSingleServer()
-                .setAddress("redis://localhost:6380");
-        // 创建RedissonClient实例
-        return Redisson.create(config);
-    }
-    @Bean
-    public RedissonClient redissonClient3(){
-        // 创建配置
-        Config config = new Config();
-        // 使用单机模式
-        config.useSingleServer()
-                .setAddress("redis://localhost:6381");
-        // 创建RedissonClient实例
-        return Redisson.create(config);
-    }
+//    @Bean
+//    public RedissonClient redissonClient2(){
+//        // 创建配置
+//        Config config = new Config();
+//        // 使用单机模式
+//        config.useSingleServer()
+//                .setAddress("redis://localhost:6380");
+//        // 创建RedissonClient实例
+//        return Redisson.create(config);
+//    }
+//    @Bean
+//    public RedissonClient redissonClient3(){
+//        // 创建配置
+//        Config config = new Config();
+//        // 使用单机模式
+//        config.useSingleServer()
+//                .setAddress("redis://localhost:6381");
+//        // 创建RedissonClient实例
+//        return Redisson.create(config);
+//    }
 
 }
